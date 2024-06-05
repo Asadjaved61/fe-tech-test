8f66ce89dda242f462e910984e2049037a00b6b1
Asadjaved61 <asad_sayyan@hotmail.com>
2024-06-05 18:11:29 +0100

    add react for ui and intial setup

--- server/main.py
+++ server/main.py
@@ -3,6 +3,7 @@ import re
 from contextlib import asynccontextmanager
 from enum import Enum
 from typing import Optional, List
+from fastapi.middleware.cors import CORSMiddleware
 
 from fastapi import FastAPI, Query
 
@@ -19,6 +20,18 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+origins = [
+    "http://localhost:3000",  # Replace with the actual origin of your frontend
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
